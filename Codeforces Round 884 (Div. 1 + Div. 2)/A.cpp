1dbdb6fe11aa36e951f0d38b24e2bc4d4af556d6
lengmuzashi <2690497440@qq.com>
2023-07-12 23:37:58 +0800

    Codeforces Round 884 (Div. 1 + Div. 2)

---
+++ Codeforces Round 884 (Div. 1 + Div. 2)/A.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//using namespace std;
+//typedef long long ll;
+//int main()
+//{
+//	ll t = 0;
+//	cin >> t;
+//	while (t--)
+//	{
+//		ll a = 0, b = 0;
+//		cin >> a >> b;
+//		cout << a + b << endl;
+//	}
+//}
