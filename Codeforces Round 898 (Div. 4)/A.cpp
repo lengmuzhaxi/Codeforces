5ceae7f5dfae7502db5802246be6baa497876e83
lengmuzashi <2690497440@qq.com>
2023-09-22 22:50:47 +0800

    Codeforces Round 898 (Div. 4)

---
+++ Codeforces Round 898 (Div. 4)/A.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int m ;
+//	cin >> m;
+//	while (m--)
+//	{
+//		string k;
+//		cin >> k;
+//		if (k == "abc" || k == "acb" || k== "bac" || k == "cba")
+//		{
+//			cout << "YES" << '\n';
+//		}
+//		else
+//		{
+//			cout << "NO" << '\n';
+//		}
+//	}
+//}
---
+++ Codeforces Round 898 (Div. 4)/B.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//int main()
+//{
+//	int t = 0;
+//	cin >> t;
+//	while (t--)
+//	{
+//		int n = 0, b[10] = { 0 }, si = 1;
+//		cin >> n;
+//		for (int i = 0; i < n; i++)
+//		{
+//			cin >> b[i];
+//		}
+//		sort(b, b + n);
+//		b[0] = b[0] + 1;
+//		for (int i = 0; i < n; i++)
+//		{
+//			si*= b[i];
+//		}
+//		cout << si << '\n';
+//	}
+//}
