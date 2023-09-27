5d008889014eae171bbe8e19f9f92343ba560a3c
lengmuzashi <2690497440@qq.com>
2023-09-27 23:44:41 +0800

    Codeforces Round #900 (Div. 3)

---
+++ Codeforces Round #900 (Div. 3)/A.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int tt= 0;
+//	cin >> tt;
+//	while (tt--)
+//	{
+//		int a[110], k = 0, n = 0,si=0;
+//		cin >> n >> k;
+//		for (int i = 0; i < n; i++)
+//		{
+//			cin >> a[i];
+//			if (a[i] == k)
+//			{
+//				si++;
+//			}
+//		}
+//		if (si >= 1)
+//		{
+//			cout << "YES\n";
+//		}
+//		else
+//		{
+//			cout << "NO\n";
+//		}
+//	}
+//}
