5cb3f0faf1f2b158f32cdc649c682a80b56eb8de
lengmuzashi <2690497440@qq.com>
2023-11-26 19:32:28 +0800

    CodeTON Round 7 (Div. 1 + Div. 2, Rated, Prizes!)

---
+++ CodeTON Round 7 (Div. 1 + Div. 2, Rated, Prizes!)/A.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//int main()
+//{
+//	int t = 0;
+//	cin >> t;
+//	while (t--)
+//	{
+//		int n = 0, a[15] = { 0 },f=1,cnt=1;
+//		cin >> n;
+//		for (int i = 0; i < n; i++)
+//		{
+//			cin >> a[i];
+//		}
+//		if (a[0] == 1)
+//		{
+//			cout << "YES\n";
+//		}
+//		else
+//		{
+//			cout << "NO\n";
+//		}
+//	}
+//}
---
+++ CodeTON Round 7 (Div. 1 + Div. 2, Rated, Prizes!)/B.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int t = 0;
+//	cin >> t;
+//	while (t--)
+//	{
+//		string s;
+//		int n = 0;
+//		cin >> n >> s;
+//		int l = 0, r = n - 1;
+//		while(s[l] =='B')
+//		{
+//			l++;
+//		}
+//		while(s[r]=='A')
+//		{
+//			r--;
+//		}
+//		if (l> r)
+//		{
+//			cout << 0 << '\n';
+//		}
+//		else
+//		{
+//			cout << r-l<<'\n';
+//		}
+//	}
+//}
