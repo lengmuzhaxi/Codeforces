da861a9a2dc02bc2adf9746759de702b27778e60
lengmuzhaxi <2690497440@qq.com>
2024-01-28 23:50:36 +0800

    Codeforces Round 921 (Div. 2)

---
+++ Codeforces Round 921 (Div. 2)/A.cpp
@@ -0,0 +1,21 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+	int t=0;
+	cin >> t;
+	while (t--)
+	{
+		string s;
+		int n, k,m=0;
+		cin >> n>>k;
+		for (int i = 0; i < n; i++)
+		{
+			for (char i = 'a'; i < 'a' + k; i++)
+			{
+				cout << i;
+			}
+		}
+		cout << '\n';
+	}
+}*/
