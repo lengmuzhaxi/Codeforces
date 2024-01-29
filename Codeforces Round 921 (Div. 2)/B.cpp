4ddb7bd993011e0f61656a08482693935dbe4f9c
lengmuzhaxi <2690497440@qq.com>
2024-01-30 00:00:41 +0800

    Codeforces Round 921 (Div. 2)

---
+++ Codeforces Round 921 (Div. 2)/B.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, n;
+        cin >> x >> n;
+        int ans = 1;
+        for (int i = 1; i * i <= x; i++)
+        {
+            if (x % i == 0)
+            {
+                if (n <= x / i)
+                {
+                    ans = max(ans, i);
+                }
+                if (n <= i)
+                {
+                    ans = max(ans, x / i);
+                }
+            }
+        }
+        cout << ans << '\n';
+    }
+}
