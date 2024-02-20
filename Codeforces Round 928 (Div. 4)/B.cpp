f26d7b6ad68499b3a33b13896d06d9e58a6a78da
lengmuzhaxi <2690497440@qq.com>
2024-02-20 23:57:25 +0800

    Codeforces Round 928 (Div. 4)

---
+++ Codeforces Round 928 (Div. 4)/B.cpp
@@ -0,0 +1,37 @@
+//#include<iostream>
+//#include<set>
+//using namespace std;
+//int main()
+//{
+//    int t;
+//    cin >> t;
+//    while (t--)
+//    {
+//        int n;
+//        set<int>a;
+//        cin >> n;
+//        for (int i = 0; i < n; i++)
+//        {
+//            int k=0;
+//            string s;
+//            cin >> s;
+//            for (int i = 0; i < n; i++)
+//            {
+//                if (s[i] == '1')
+//                {
+//                    k++;
+//                }
+//            }
+//            if(k>0)
+//            a.insert(k);
+//        }
+//        if (a.size() == 1)
+//        {
+//            cout << "SQUARE\n";
+//        }
+//        else
+//        {
+//            cout << "TRIANGLE\n";
+//        }
+//    }
+//}
