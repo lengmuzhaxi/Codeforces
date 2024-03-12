6778a1dd27f9c3ab0bea4b800a04892f65edec34
lengmuzhaxi <2690497440@qq.com>
2024-03-12 23:13:10 +0800

    Codeforces Round 933 (Div. 3)

---
+++ Codeforces Round 933 (Div. 3)/A.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int tt;
+//	cin >> tt;
+//	while (tt--)
+//	{
+//		int n, m, p,cn=0;
+//		cin >> n >> m >> p;
+//		int a[105] , b[105] ;
+//		for (int i = 0; i < n; i++)
+//		{
+//			cin >> a[i];
+//		}
+//		for (int i = 0; i < m; i++)
+//		{
+//			cin >> b[i];
+//		}
+//		for (int i = 0; i < n; i++)
+//		{
+//			
+//			for (int j = 0; j < m; j++)
+//			{
+//				if (a[i] + b[j] <=p)
+//				{
+//					cn++;
+//				}
+//			}
+//		}
+//		cout << cn << '\n';
+//	}
+//}
