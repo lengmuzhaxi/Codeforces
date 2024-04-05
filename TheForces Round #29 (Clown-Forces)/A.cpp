c1d93c6eba11acc5a9d34609c2cb39ac759d321f
lengmuzhaxi <2690497440@qq.com>
2024-04-05 23:13:31 +0800

    TheForces Round #29 (Clown-Forces)

---
+++ TheForces Round #29 (Clown-Forces)/A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m,n;
+    cin>>n>>m;
+    if(m==1&&n==4)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"No";
+    }
+}
