201f8f313611eab09ac9178033b4eef4a509af44
lengmuzhaxi <2690497440@qq.com>
2024-05-14 22:14:27 +0800

    Codeforces Round 944 (Div. 4)

---
+++ Codeforces Round 944 (Div. 4)/A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<min(a,b)<<' '<<max(a,b)<<'\n';
+    }
+}//
+// Created by dzl on 24-5-14.
+//
