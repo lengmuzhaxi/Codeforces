b1a57bef40f54e171261867711af033a4c0f164d
lengmuzhaxi <2690497440@qq.com>
2024-05-31 23:58:57 +0800

    Codeforces Round 949 (Div. 2)

---
+++ Codeforces Round 949 (Div. 2)/A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int l,r;
+        cin>>l>>r;
+        cout<<(int)log2(r)<<endl;
+    }
+}//
+// Created by dzl on 24-5-31.
+//
