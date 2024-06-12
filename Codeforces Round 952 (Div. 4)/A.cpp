3d976b4fd18d21f05cc6976a31df023739d1bb3b
lengmuzhaxi <2690497440@qq.com>
2024-06-12 21:54:34 +0800

    Codeforces Round 952 (Div. 4)

---
+++ Codeforces Round 952 (Div. 4)/A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a,b;
+        cin>>a>>b;
+        char c=a[0];
+        a[0]=b[0];
+        b[0]=c;
+        cout<<a<<' '<<b<<'\n';
+    }
+}
