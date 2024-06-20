560a9dc1f7518525d8f864155137b9b5cbb33e74
lengmuzhaxi <2690497440@qq.com>
2024-06-20 23:05:46 +0800

    Codeforces Round 952 (Div. 4)

---
+++ Codeforces Round 952 (Div. 4)/C.cpp
@@ -0,0 +1,25 @@
+/*
+#include<bits/stdc++.h>
+long long int t,m,n,k,d,l;
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin>>t;
+    while(t--){
+        cin>>m;
+        n=0;
+        d=0;
+        l=0;
+        for(int i=0;i<m;i++){
+            cin>>k;
+            n+=k;
+            if(k>d) d=k;
+            if(n==2*d) l++;
+        }
+        cout<<l<<"\n";
+    }
+    return 0;
+}
+*/
