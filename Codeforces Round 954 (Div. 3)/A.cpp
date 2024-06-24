e28eabef64d6fd95ed96fd97446dda47bf06d683
lengmuzhaxi <2690497440@qq.com>
2024-06-24 23:55:50 +0800

    Codeforces Round 954 (Div. 3)

---
+++ Codeforces Round 954 (Div. 3)/A.cpp
@@ -0,0 +1,16 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        int k=abs(a-b)+abs(a-c);
+        int x=abs(b-a)+abs(b-c);
+        int y=abs(c-a)+abs(c-b);
+        cout<<min(k,min(x,y))<<'\n';
+    }
+}*/
