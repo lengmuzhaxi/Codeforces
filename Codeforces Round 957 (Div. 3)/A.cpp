efb4e93e61ec68d68adb6632ab032e72a25f5451
lengmuzhaxi <2690497440@qq.com>
2024-07-12 21:44:11 +0800

    Codeforces Round 957 (Div. 3)

---
+++ Codeforces Round 957 (Div. 3)/A.cpp
@@ -0,0 +1,19 @@
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int a[3];
+        cin>>a[0]>>a[1]>>a[2];
+        for(int i=0;i<5;i++){
+            sort(a,a+3);
+            a[0]++;
+        }
+        cout<<a[0]*a[1]*a[2]<<endl;
+    }
+    return 0;
+}
+*/
