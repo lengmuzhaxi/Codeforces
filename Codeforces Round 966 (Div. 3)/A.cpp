7103fa1f11e1e2d8f7c68c2629fc6dbeaae53bbd
lengmuzhaxi <2690497440@qq.com>
2024-08-14 23:47:04 +0800

    Codeforces Round 966 (Div. 3)

---
+++ Codeforces Round 966 (Div. 3)/A.cpp
@@ -0,0 +1,33 @@
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+#include<string>
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        if(s.size()>=3)
+        {
+            string k=s.substr(2,s.size());
+            if((s.substr(0,2)=="10")&&(k[0]!='0')&&(std::stoi(k)>=2))
+            {
+                cout<<"YES\n";
+            }
+            else
+            {
+                cout<<"NO\n";
+            }
+        }
+        else
+        {
+            cout<<"NO\n";
+        }
+    }
+    return 0;
+}
+*/
+
