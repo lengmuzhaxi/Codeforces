e79852e992f4faa4cd94839747937981128e6d2d
lengmuzhaxi <2690497440@qq.com>
2024-08-17 23:13:26 +0800

    Educational Codeforces Round 169 (Rated for Div. 2)

---
+++ Educational Codeforces Round 169 (Rated for Div. 2)/A.cpp
@@ -0,0 +1,13 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int t,n,m,k,a[200005];
+int main(){
+    cin>>t;
+    while(t--){
+        cin>>n;
+        for(int i=1;i<=n;i++)cin>>a[i];
+        if(n>2||abs(a[1]-a[2])==1)cout<<"NO\n";
+        else cout<<"YES\n";
+    }
+    return 0;
+}*/
